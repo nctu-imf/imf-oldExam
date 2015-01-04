0ad8f5caea76f7b77ded104cfe2c8800c02cd3fd
chichunchen <chi.chun_chen844@icloud.com>
2015-01-05 05:20:52 +0800

    add simple navbar and footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap/dropdown
+//= require bootstrap/alert
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # root 'welcome#index'
+  root 'home#index'
 
 end
