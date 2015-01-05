6b24b5dc6c733c3416d9706c6ef1a8162d48a937
chichunchen <chi.chun_chen844@icloud.com>
2015-01-06 02:41:06 +0800

    add automation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+create_account = User.create([email: 'example@gmail.com', 
+                password: '12345678', 
+                password_confirmation: '12345678'])
+
+category_list = ["期中考", "期末考", "小考", "上機考"]
+
+create_course = for i in 1..20 do
+          Course.create!([
+                name: "種子建立的第#{i}門課", 
+                category: "#{category_list[rand(category_list.length)]}",
+                year: "#{2010 + rand(5)}", 
+                grade: "#{1 + rand(3)}", 
+                teacher: "教授NO.#{i}",
+                note: "備註#{i}"
+              ])
+        end
