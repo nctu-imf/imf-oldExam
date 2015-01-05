3ffbf8ee08939db1c07bcc227ab3d2b2740d36d4
chichunchen <chi.chun_chen844@icloud.com>
2015-01-06 02:08:34 +0800

    course validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,13 @@
 class Course < ActiveRecord::Base
+  
+  validates :year, :name, :teacher, :grade, :category, :presence => true
+  
+  validates :grade, :inclusion => 1..4
+
+  validates :year,
+    inclusion: { in: 1990..Date.today.year },
+    format: { 
+      with: /(19|20)\d{2}/i, 
+      message: "請輸入合法的年份"
+    }
 end
