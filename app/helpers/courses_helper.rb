e11bc079e6f076a065963154901af4c24aaf35b9
chichunchen <chi.chun_chen844@icloud.com>
2015-01-27 01:22:22 +0800

    course add modified state

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -43,6 +43,8 @@ module CoursesHelper
   def render_course_fresh_state(course)
     if course.created_at >= Date.today - 1.days
       content_tag(:span, "NEW", :class => "label label-success")
+    elsif course.updated_at >= Date.today - 1.days
+      content_tag(:span, "MOD", :class => "label label-warning")
     end
   end
 end
