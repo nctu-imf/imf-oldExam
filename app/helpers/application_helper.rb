3dba83d4b221a49608f51f3f80655701d969663f
chi chun chen <chi.chun_chen844@icloud.com>
2015-02-02 00:08:07 +0800

    create event should not appear in diff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,11 +36,11 @@ module ApplicationHelper
 
   def markdown(text)
     render_options = {
-      # will remove from the output HTML tags inputted by user 
+      # will remove from the output HTML tags inputted by user
       filter_html:     true,
-      # will insert <br /> tags in paragraphs where are newlines 
+      # will insert <br /> tags in paragraphs where are newlines
       # (ignored by default)
-      hard_wrap:       true, 
+      hard_wrap:       true,
       # hash for extra link options, for example 'nofollow'
       link_attributes: { rel: 'nofollow' }
       # more
@@ -59,7 +59,7 @@ module ApplicationHelper
     extensions = {
       #will parse links without need of enclosing them
       autolink:           true,
-      # blocks delimited with 3 ` or ~ will be considered as code block. 
+      # blocks delimited with 3 ` or ~ will be considered as code block.
       # No need to indent.  You can provide language name too.
       # ```ruby
       # block of code
@@ -71,7 +71,7 @@ module ApplicationHelper
       no_intra_emphasis:  true,
       # will parse strikethrough from ~~, for example: ~~bad~~
       strikethrough:      true,
-      # will parse superscript after ^, you can wrap superscript in () 
+      # will parse superscript after ^, you can wrap superscript in ()
       superscript:        true
       # will require a space after # in defining headers
       # space_after_headers: true
